4e13ca9e59453fc9c465ce084ba56d032d6cd956
kinsleykajiva <kajivakinsley@gmail.com>
2020-01-02 09:57:05 +0200

    works on recognising faces with faces and ids

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
+            name = const.UNKNOWN
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
@@ -89,7 +89,7 @@ while True:
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
         roi_color = frame[top:top + left, top:top + bottom]
-        if name == 'Unknown':
+        if name == const.UNKNOWN:
             millis = int(round(time.time() * 1000))
             millis = 'kil' + str(millis)
             # print('jik' + millis)
