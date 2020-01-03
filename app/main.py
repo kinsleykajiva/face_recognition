27b7212ef0aa72cc352c39f22e3afa7bf277ea2c
kinsleykajiva <kajivakinsley@gmail.com>
2020-01-03 09:22:23 +0200

    working managed to make it remember from preview data. but works properly now, still have to make other adidditions to the system

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-import face_recognition
-import cv2
-import time
-import numpy as np
-import constants as const
 import os
+import time
+
+import constants as const
+import cv2
+import face_recognition
+import numpy as np
 
 NEWFACES = []
 toRun = True
 
 
 def loadFacesImages(listOfImages: list):
-
     loaded_encodes = []
     loaded_labels = []
     # print(listOfImages)
@@ -18,7 +18,7 @@ def loadFacesImages(listOfImages: list):
     for img in listOfImages:
         if os.path.exists(img):
             # Load a sample picture and learn how to recognize it.
-            load = face_recognition.load_image_file( img)
+            load = face_recognition.load_image_file(img)
             encode = face_recognition.face_encodings(load)
             if len(encode) != 0:
                 encode = encode[0]
@@ -77,15 +77,13 @@ def runRecognition(known_face_names, known_face_encodings):
             if name == const.UNKNOWN:
                 NEWFACES.append(saveUnknownImage(small_frame))
 
-
-
             # Draw a label with a name below the face
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            print ('Status::'+name)
+            print('Status::' + name)
         if len(NEWFACES) > 3:
-            print('loading new ...' )
+            print('loading new ...')
             reloadWithNewImages()
 
         # Display the resulting image
@@ -96,33 +94,34 @@ def runRecognition(known_face_names, known_face_encodings):
 
 
 def reloadWithNewImages():
-        known_face_names, known_face_encodings =loadFacesImages(NEWFACES)
+    known_face_names, known_face_encodings = loadFacesImages(NEWFACES)
 
-        runRecognition(known_face_names, known_face_encodings)
-
+    runRecognition(known_face_names, known_face_encodings)
 
 
 def saveUnknownImage(imageObject):
     millis = int(round(time.time() * 1000))
-    millis = 'newfaces/' +'Hiil' + str(millis)  + '.png'
-    cv2.imwrite( millis, imageObject)
+    millis = 'newfaces/' + 'Hiil' + str(millis) + '.png'
+    cv2.imwrite(millis, imageObject)
     return millis
 
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-#load existing files
 
-for r,d,f in os.walk('newfaces/'):
-    for file in f:
-        NEWFACES.append(os.path.join(r,file))
+# load existing files
+def loadFrom(folders: list):
+    files = []
+    for folder in folders:
+        for r, d, f in os.walk(folder + '/'):
+            for file in f:
+                files.append(os.path.join(r, file))
+    return files
 
-for r,d,f in os.walk('imgs/'):
-    for file in f:
-        NEWFACES.append(os.path.join(r,file))
 
-print (NEWFACES)
+NEWFACES = NEWFACES + loadFrom(['imgs/', 'newfaces/'])
+# print(NEWFACES)
 known_face_names, known_face_encodings = loadFacesImages(NEWFACES)
 runRecognition(known_face_names, known_face_encodings)
 # Release handle to the webcam
